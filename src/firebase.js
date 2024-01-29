77476f2e13db649a9b2efce6616331ebddedcc31
Ethanx0813 <ankityadav770587@gmail.com>
2024-01-29 22:55:16 +0530

    firebae

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,16 @@
    import 'firebase/auth';
 
    const firebaseConfig = {
-     apiKey: 'YOUR_API_KEY',
-     authDomain: 'YOUR_AUTH_DOMAIN',
-     projectId: 'YOUR_PROJECT_ID',
-     storageBucket: 'YOUR_STORAGE_BUCKET',
-     messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-     appId: 'YOUR_APP_ID',
-   };
+    apiKey: "AIzaSyCIKivrHCo3XYEBc45aW0EIhlcg-nnKaEk",
+    authDomain: "react-ecommerce-store-8c372.firebaseapp.com",
+    projectId: "react-ecommerce-store-8c372",
+    storageBucket: "react-ecommerce-store-8c372.appspot.com",
+    messagingSenderId: "591455354616",
+    appId: "1:591455354616:web:9127b5ca220e6ad3b8850d",
+    databaseURL :"https://react-ecommerce-store-8c372-default-rtdb.firebaseio.com",
+  }
+
+   ;
 
    // Initialize Firebase
    firebase.initializeApp(firebaseConfig);
